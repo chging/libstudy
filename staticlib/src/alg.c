6a3fc5034bf8874d48c720ef52681496c8b3ca45
chging <chg_ly@163.com>
2016-07-09 09:30:13 +0800

    a program to study linux static library

---
+++ staticlib/src/alg.c
@@ -0,0 +1,18 @@
+
+#include "alg.h"
+
+static int s_i_num = 0;
+const double c_d_num = 1;
+const static int c_s_i_num = 3;
+
+
+
+int Add(int a, int b)
+{
+  return a+b;  
+}
+
+int Sub(int a, int b)
+{
+  return a-b;
+}
---
+++ staticlib/src/main.c
@@ -0,0 +1,18 @@
+#include "alg.h"
+
+#define NUM 10;
+
+int main()
+{
+  int a = 10, b = 125, sum = 0;
+
+  sum = Add(a, b);
+
+
+  for(int i = 0; i < 10; ++i)
+  {
+  
+  }
+
+  return 0;
+}
